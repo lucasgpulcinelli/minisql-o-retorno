17313cd4e5478cba1ad88d5c0a3af2600a910015
gabriel-libardi <libardi.120205@protonmail.com>
2022-10-17 23:06:31 -0300

    Debugged readEntryFromStdin

--- src/inputs.c
+++ src/inputs.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
+#include "inputs.h"
 #include "utils.h"
 #include "entries.h"
+
+static const int8_t data_types_map[] = {
+    boolean, integer, integer, char_array, integer, 
+    char_array, integer, char_p, char_p
+};
 
 
 field* readTuples(int n){
@@ -84,4 +91,96 @@ void freeTuples(field* fs, int n){
         }
     }
     free(fs);
+}
+
+void readEntryFromCSV(char *csv_line, entry *es){
+    char *field = (char *)strtok(csv_line, ",");
+    size_t num_fields = 0;
+
+    for(num_fields = 0; field && num_fields < FIELD_AMOUNT; 
+        num_fields++, field = (char *)strtok(NULL, ",")) {
+        switch(data_types_map[num_fields]){
+            case boolean:
+                es->fields[num_fields].value.cbool = atoi(field);
+                break;
+            
+            case integer:
+                es->fields[num_fields].value.integer = atoi(field);
+                break;
+            
+            case char_array:
+                strncpy(es->fields[num_fields].value.carray, field, CHAR_ARRAY_LEN);
+                break;
+
+            case char_p:
+                XALLOC(char, es->fields[num_fields].value.cpointer, strlen(field) + 1);
+                strcpy(es->fields[num_fields].value.cpointer, field);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    if(num_fields < FIELD_AMOUNT) {
+        errno = EINVAL;
+        ABORT_PROGRAM("Bad number of fields: found %lu in input, "
+                      "but there should be %d",
+                      num_fields, FIELD_AMOUNT);
+
+    } else if(field) {
+        errno = EINVAL;
+        ABORT_PROGRAM("Bad number of fields: found more then"
+                      " %lu in input.", FIELD_AMOUNT);
+    }
+}
+
+void readEntryFromStdin(entry *es){
+    static const int8_t order[] = {
+        removed, linking, idConnect, poPsName, countryName, 
+        countryAcro, connPoPsId, measurmentUnit, speed
+    };
+
+    for(size_t field = idConnect; field < FIELD_AMOUNT; field++){
+        char* field_str;
+        READ_INPUT("%ms", &field_str);
+
+        int8_t data_type = data_types_map[order[field]];
+        if(!strcmp(field_str, "NULO")){
+           data_type += NULL_OFFSET;
+        }
+
+        switch (data_type){
+        case integer:
+            es->fields[order[field]].value.integer = atoi(field_str);
+            break;
+        
+        case char_array:
+            strncat(es->fields[order[field]].value.carray, field_str, 
+                    CHAR_ARRAY_SIZE);
+            break;
+        
+        case char_p:
+            ssize_t str_size = strlen(field_str);
+            XALLOC(char, es->fields[order[field]].value.cpointer, str_size - 1);
+
+            strncpy(es->fields[order[field]].value.cpointer, field_str + 1, str_size - 2);
+            es->fields[order[field]].value.cpointer[str_size - 2] = '\0';
+            break;
+        
+        case null_int:
+            es->fields[order[field]].value.integer = NULL_INT;
+            break;
+        
+        case null_char_array:
+            strncpy(es->fields[order[field]].value.carray, "$$$$", 
+                    CHAR_ARRAY_SIZE);
+            break;
+        case null_char_p:
+            es->fields[order[field]].value.cpointer = NULL;
+            break;
+        }
+
+        free(field_str);
+    }
 }
--- src/inputs.h
+++ src/inputs.h
@@ -3,6 +3,16 @@
 
 #include "entries.h"
 
+#define NULL_INT -1
+#define CHAR_ARRAY_SIZE 4
+#define NULL_OFFSET 3
+
+enum NullTypes {
+    null_int = 4,
+    null_char_p,
+    null_char_array
+};
+
 /*
  * readTuples reads n lines of key-value pairs with field names followed by 
  * values, returning the associated fields.
@@ -11,5 +21,17 @@ field* readTuples(int n);
 
 //freeTuples frees a collection of tuples alloced by readTuples
 void freeTuples(field* fs, int n);
+
+/*
+ * readEntryFromCSV reads an entry from the string csv_line
+ * that must follow a csv ("comma separated values") format.
+ */
+void readEntryFromCSV(char *csv_line, entry *es);
+
+/*
+ * readEntryFromStdin reads an entry from stdin in format
+ * specified by commandInsert.
+ */
+void readEntryFromStdin(entry *es);
 
 #endif
